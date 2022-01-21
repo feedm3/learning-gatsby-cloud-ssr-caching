cc31334cc6b329a1b8e8ef28de05678230ad1f25
Fabian Dietenberger <fabian.dietenberger@satellytes.com>
2022-01-21 07:51:27 +0100

    feat: wait 5 seconds to get the server data

--- src/pages/ssr.js
+++ src/pages/ssr.js
@@ -10,6 +10,7 @@ export default function SSR(props) {
       <Link to='/'>Home</Link><br/>
       <h1>SSR: Server Side Rendering</h1>
       <p>Page created at: {createdDate}</p>
+      <p>This page took 5 seconds to be returned from the server</p>
       <img
         alt='doggo'
         src={image}
@@ -19,10 +20,14 @@ export default function SSR(props) {
 }
 
 export async function getServerData({params}) {
-  console.log('getServerData');
+  console.log('Request incoming...');
 
   const data = await fetch(`https://dog.ceo/api/breeds/image/random`)
     .then(res => res.json())
+
+  await new Promise(resolve => setTimeout(resolve, 5000));
+
+  console.log('Request done. Returning data...');
 
   return {
     headers: {
